d794177c87351996e906b936c3b99ded34c039ad
Monil Shah <shah2004monil@gmail.com>
2025-07-22 11:32:23 +0530

    Point root to Game component; remove default index

---
+++ pages/index.jsx
@@ -0,0 +1,7 @@
+// pages/index.jsx
+import React from 'react'
+import Game from './game'    // assumes your game logic lives in pages/game.jsx
+
+export default function Home() {
+  return <Game />
+}
